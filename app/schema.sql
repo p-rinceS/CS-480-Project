a4e0b87fd16d762fb1a2ddf8fdbe2ba4a7a647a0
junyoung-j <jjeon28@uic.edu>
2025-04-18 23:48:15 -0500

    All tables are in taxischema

--- app/schema.sql
+++ app/schema.sql
@@ -1,80 +1,82 @@
-CREATE TABLE address (
+CREATE SCHEMA taxischema;
+
+CREATE TABLE taxischema.address (
     road TEXT NOT NULL,
     number INTEGER NOT NULL,
     city TEXT NOT NULL,
     PRIMARY KEY (road, number, city)
 );
 
-CREATE TABLE client (
+CREATE TABLE taxischema.client (
     name TEXT NOT NULL,
     email TEXT NOT NULL,
     home_road TEXT NOT NULL,
     home_number INTEGER NOT NULL,
     home_city TEXT NOT NULL,
-    CONSTRAINT fk_client_address FOREIGN KEY (home_road, home_number, home_city) REFERENCES address(road, number, city) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_client_address FOREIGN KEY (home_road, home_number, home_city) REFERENCES taxischema.address(road, number, city) ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY (email)
 );
 
-CREATE TABLE credit_card (
+CREATE TABLE taxischema.credit_card (
     number TEXT NOT NULL,
     client_email TEXT NOT NULL,
     billing_road TEXT NOT NULL,
     billing_number INTEGER NOT NULL,
     billing_city TEXT NOT NULL,
-    CONSTRAINT fk_card_client FOREIGN KEY (client_email) REFERENCES client(email) ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT fk_card_address FOREIGN KEY (billing_road, billing_number, billing_city) REFERENCES address(road, number, city) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_card_client FOREIGN KEY (client_email) REFERENCES taxischema.client(email) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_card_address FOREIGN KEY (billing_road, billing_number, billing_city) REFERENCES taxischema.address(road, number, city) ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY (number)
 );
 
-CREATE TABLE driver (
+CREATE TABLE taxischema.driver (
     name TEXT NOT NULL,
     home_road TEXT NOT NULL,
     home_number INTEGER NOT NULL,
     home_city TEXT NOT NULL,
-    CONSTRAINT fk_driver_address FOREIGN KEY (home_road, home_number, home_city) REFERENCES address(road, number, city) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_driver_address FOREIGN KEY (home_road, home_number, home_city) REFERENCES taxischema.address(road, number, city) ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY (name)
 );
 
-CREATE TABLE review (
+CREATE TABLE taxischema.review (
     review_id SERIAL,
     message TEXT NOT NULL,
     rating INT CHECK (rating BETWEEN 1 AND 5),
     client_email TEXT NOT NULL,
     driver_name TEXT NOT NULL,
-    CONSTRAINT fk_review_client FOREIGN KEY (client_email) REFERENCES client(email) ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT fk_review_driver FOREIGN KEY (driver_name) REFERENCES driver(name) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_review_client FOREIGN KEY (client_email) REFERENCES taxischema.client(email) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_review_driver FOREIGN KEY (driver_name) REFERENCES taxischema.driver(name) ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY (review_id, client_email)
 );
 
-CREATE TABLE rent (
+CREATE TABLE taxischema.rent (
     rent_id SERIAL,
     date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     client_email TEXT NOT NULL,
     driver_name TEXT NOT NULL,
-    CONSTRAINT fk_rent_client FOREIGN KEY (client_email) REFERENCES client(email) ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT fk_rent_driver FOREIGN KEY (driver_name) REFERENCES driver(name) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_rent_client FOREIGN KEY (client_email) REFERENCES taxischema.client(email) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_rent_driver FOREIGN KEY (driver_name) REFERENCES taxischema.driver(name) ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY (rent_id)
 );
 
-CREATE TABLE car (
+CREATE TABLE taxischema.car (
     car_id SERIAL,
     brand TEXT NOT NULL,
     PRIMARY KEY (car_id)
 );
 
-CREATE TABLE model (
+CREATE TABLE taxischema.model (
     model_id SERIAL,
     color TEXT NOT NULL,
     year INT NOT NULL,
     transmission TEXT NOT NULL,
     car_id INT NOT NULL,
     rent_id INT NOT NULL,
-    CONSTRAINT fk_model_car FOREIGn KEY (car_id) REFERENCES car(car_id) ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT fk_model_rent FOREIGN KEY (rent_id) REFERENCES rent(rent_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_model_car FOREIGn KEY (car_id) REFERENCES taxischema.car(car_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_model_rent FOREIGN KEY (rent_id) REFERENCES taxischema.rent(rent_id) ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY (model_id, car_id)
 );
 
-CREATE TABLE manager (
+CREATE TABLE taxischema.manager (
     ssn CHAR NOT NULL,
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
@@ -82,22 +84,22 @@ CREATE TABLE manager (
 );
 
 -- N:M relationship between driver and model
-CREATE TABLE driver_model (
+CREATE TABLE taxischema.driver_model (
     driver_name TEXT NOT NULL,
     model_id INT NOT NULL,
     car_id INT NOT NULL,
-    CONSTRAINT fk_join_driver FOREIGN KEY (driver_name) REFERENCES driver(name) ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT fk_join_model FOREIGN KEY (model_id, car_id) REFERENCES model(model_id, car_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_join_driver FOREIGN KEY (driver_name) REFERENCES taxischema.driver(name) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_join_model FOREIGN KEY (model_id, car_id) REFERENCES taxischema.model(model_id, car_id) ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY (driver_name, model_id, car_id)
 );
 
 -- N:M relationship between client and address
-CREATE TABLE client_address (
+CREATE TABLE taxischema.client_address (
     client_email TEXT NOT NULL,
     home_road TEXT NOT NULL,
     home_number INTEGER NOT NULL,
     home_city TEXT NOT NULL,
-    CONSTRAINT fk_join_client FOREIGN KEY (client_email) REFERENCES client(email) ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT fk_join_address FOREIGN KEY (home_road, home_number, home_city) REFERENCES address(road, number, city) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_join_client FOREIGN KEY (client_email) REFERENCES taxischema.client(email) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_join_address FOREIGN KEY (home_road, home_number, home_city) REFERENCES taxischema.address(road, number, city) ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY (client_email, home_road, home_number, home_city)
 );
