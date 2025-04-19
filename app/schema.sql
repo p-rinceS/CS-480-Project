c2d80c801e37dfe6d98395d4cf19fa9d6e21eaf6
junyoung-j <jjeon28@uic.edu>
2025-04-18 23:45:26 -0500

    Added foreign keys and tables for binary relationships and rearranged for compilation

--- app/schema.sql
+++ app/schema.sql
@@ -1,92 +1,103 @@
-CREATE SCHEMA taxiSchema;
-
 CREATE TABLE address (
-    address_id SERIAL,
-    road_number INTEGER NOT NULL,
-    city VARCHAR NOT NULL,
-    PRIMARY KEY (address_id)
-);
-
-CREATE TABLE credit_card (
-    card_id SERIAL,
-    number VARCHAR UNIQUE NOT NULL,
-    client_id INT NOT NULL,
-    CONSTRAINT fk_card_client FOREIGN KEY (client_id) REFERENCES client(client_id),
-    PRIMARY KEY (card_id)
+    road TEXT NOT NULL,
+    number INTEGER NOT NULL,
+    city TEXT NOT NULL,
+    PRIMARY KEY (road, number, city)
 );
 
 CREATE TABLE client (
-    client_id SERIAL,
-    name VARCHAR NOT NULL,
-    email VARCHAR UNIQUE NOT NULL,
-    card_id INT UNIQUE NOT NULL,
-    address_id INT NOT NULL,
-    PRIMARY KEY (client_id),
-    CONSTRAINT fk_client_address FOREIGN KEY (address_id) REFERENCES address(address_id),
-    CONSTRAINT fk_client_card FOREIGN KEY (card_id) REFERENCES credit_card(card_id)
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    home_road TEXT NOT NULL,
+    home_number INTEGER NOT NULL,
+    home_city TEXT NOT NULL,
+    CONSTRAINT fk_client_address FOREIGN KEY (home_road, home_number, home_city) REFERENCES address(road, number, city) ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY (email)
+);
+
+CREATE TABLE credit_card (
+    number TEXT NOT NULL,
+    client_email TEXT NOT NULL,
+    billing_road TEXT NOT NULL,
+    billing_number INTEGER NOT NULL,
+    billing_city TEXT NOT NULL,
+    CONSTRAINT fk_card_client FOREIGN KEY (client_email) REFERENCES client(email) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_card_address FOREIGN KEY (billing_road, billing_number, billing_city) REFERENCES address(road, number, city) ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY (number)
 );
 
 CREATE TABLE driver (
-    driver_id SERIAL,
-    name VARCHAR NOT NULL,
-    manager VARCHAR NOT NULL,
-    address_id INT NOT NULL,
-    PRIMARY KEY (driver_id),
-    CONSTRAINT fk_driver_address FOREIGN KEY (address_id) REFERENCES address(address_id)
+    name TEXT NOT NULL,
+    home_road TEXT NOT NULL,
+    home_number INTEGER NOT NULL,
+    home_city TEXT NOT NULL,
+    CONSTRAINT fk_driver_address FOREIGN KEY (home_road, home_number, home_city) REFERENCES address(road, number, city) ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY (name)
 );
 
 CREATE TABLE review (
     review_id SERIAL,
     message TEXT NOT NULL,
     rating INT CHECK (rating BETWEEN 1 AND 5),
-    client_id INT NOT NULL,
-    driver_id INT NOT NULL,
-    PRIMARY KEY (review_id, client_id),
-    CONSTRAINT fk_review_client FOREIGN KEY (client_id) REFERENCES client(client_id),
-    CONSTRAINT fk_review_driver FOREIGN KEY (driver_id) REFERENCES driver(driver_id)
+    client_email TEXT NOT NULL,
+    driver_name TEXT NOT NULL,
+    CONSTRAINT fk_review_client FOREIGN KEY (client_email) REFERENCES client(email) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_review_driver FOREIGN KEY (driver_name) REFERENCES driver(name) ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY (review_id, client_email)
 );
 
 CREATE TABLE rent (
     rent_id SERIAL,
     date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    client_id INT NOT NULL,
-    driver_id INT NOT NULL,
-    CONSTRAINT fk_rent_client FOREIGN KEY (client_id) REFERENCES client(client_id),
-    CONSTRAINT fk_rent_driver FOREIGN KEY (driver_id) REFERENCES driver(driver_id),
+    client_email TEXT NOT NULL,
+    driver_name TEXT NOT NULL,
+    CONSTRAINT fk_rent_client FOREIGN KEY (client_email) REFERENCES client(email) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_rent_driver FOREIGN KEY (driver_name) REFERENCES driver(name) ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY (rent_id)
-);
-
-CREATE TABLE model (
-    model_id SERIAL,
-    color VARCHAR NOT NULL,
-    year INT NOT NULL,
-    transmission VARCHAR NOT NULL,
-    car_id INT NOT NULL,
-    rent_id INT NOT NULL,
-    PRIMARY KEY (model_id, car_id),
-    CONSTRAINT fk_model_rent FOREIGN KEY (rent_id) REFERENCES rent(rent_id)
 );
 
 CREATE TABLE car (
     car_id SERIAL,
-    brand VARCHAR NOT NULL,
-    model_id INT NOT NULL,
-    driver_id INT NOT NULL,
+    brand TEXT NOT NULL,
     PRIMARY KEY (car_id)
 );
 
--- N:M relationship between driver and car
-CREATE TABLE driver_car (
-    PRIMARY KEY (driver_id, car_id),
-    driver_id INT NOT NULL,
+CREATE TABLE model (
+    model_id SERIAL,
+    color TEXT NOT NULL,
+    year INT NOT NULL,
+    transmission TEXT NOT NULL,
     car_id INT NOT NULL,
-    FOREIGN KEY (driver_id) REFERENCES driver(driver_id),
-    FOREIGN KEY (car_id) REFERENCES car(car_id)
+    rent_id INT NOT NULL,
+    CONSTRAINT fk_model_car FOREIGn KEY (car_id) REFERENCES car(car_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_model_rent FOREIGN KEY (rent_id) REFERENCES rent(rent_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY (model_id, car_id)
 );
 
 CREATE TABLE manager (
-    ssn CHAR NOT NULL PRIMARY KEY,
-    name VARCHAR NOT NULL,
-    email VARCHAR UNIQUE NOT NULL
+    ssn CHAR NOT NULL,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    PRIMARY KEY (ssn)
 );
 
+-- N:M relationship between driver and model
+CREATE TABLE driver_model (
+    driver_name TEXT NOT NULL,
+    model_id INT NOT NULL,
+    car_id INT NOT NULL,
+    CONSTRAINT fk_join_driver FOREIGN KEY (driver_name) REFERENCES driver(name) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_join_model FOREIGN KEY (model_id, car_id) REFERENCES model(model_id, car_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY (driver_name, model_id, car_id)
+);
+
+-- N:M relationship between client and address
+CREATE TABLE client_address (
+    client_email TEXT NOT NULL,
+    home_road TEXT NOT NULL,
+    home_number INTEGER NOT NULL,
+    home_city TEXT NOT NULL,
+    CONSTRAINT fk_join_client FOREIGN KEY (client_email) REFERENCES client(email) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_join_address FOREIGN KEY (home_road, home_number, home_city) REFERENCES address(road, number, city) ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY (client_email, home_road, home_number, home_city)
+);
